c447978ffec1f8a5280be0de926e023fa554c55d
wenxianping <931128603@qq.com>
2018-12-02 14:02:06 +0800

    1

--- Comment.py
+++ Comment.py
@@ -33,8 +33,9 @@ class commentThread(threading.Thread):
             movie = eval(self.redisConn.rpop("movice").decode())
             print(movie)
             offset = movie.get("offset", 0)
-            start_time = movie.get("spider_time", datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))  # 获取当前时间，从当前时间向前获取
-            while True:
+            # start_time = movie.get("spider_time", datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))  # 获取当前时间，从当前时间向前获取
+            start_time = self.redisConn.get(movie["nm"]) or datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # 获取当前时间，从当前时间向前获取
+            while start_time != "done":
                 try:
                     commentUrls = copy.copy(urls["comments"])
                     commentUrls["req_url"] = commentUrls["req_url"].format(movie.get("id"), offset, start_time)
@@ -47,11 +48,10 @@ class commentThread(threading.Thread):
                             seconds=-1)  # 转换为datetime类型，减1秒，避免获取到重复数据
                         start_time = datetime.datetime.strftime(start_time, '%Y-%m-%d %H:%M:%S')  # 转换为str
                         print(f"下次爬取评论的时间为：{start_time}")
-
-                        movie["spider_time"] = [start_time]
-                        self.redisConn.lpush("movice", movie)
+                        self.redisConn.set(movie["nm"], start_time)
                     elif getCommnetRsp.get("total", "") == 0 or getCommnetRsp.get("cmts", "") == []:  # 如果不返回数据，就代表评论爬到底
                         print("当前页面返回数据为0，判断爬取完成")
+                        self.redisConn.set(movie["nm"], "done")
                         break
                 except ValueError as e:
                     print(f"日期转化失败: {e}")
@@ -67,7 +67,7 @@ class commentThread(threading.Thread):
 
 if __name__ == '__main__':
     threadingPool = []
-    for i in range(5):
+    for i in range(8):
         u = commentThread()
         threadingPool.append(u)
     for t in threadingPool:
